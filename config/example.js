73f7b32e75670eae0d50ca5a722c2165b68657ef
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-01-03 10:42:22 -0500

    added example config file to make setup easier for everyone

---
+++ config/example.js
@@ -0,0 +1,6 @@
+// check slack for values to fill in below, then save this file as twitter.js
+
+window.CONSUMER_KEY = 'FILL_ME_IN';
+window.CONSUMER_SECRET = 'FILL_ME_IN';
+window.ACCESS_TOKEN = 'FILL_ME_IN';
+window.ACCESS_TOKEN_SECRET = 'FILL_ME_IN';
