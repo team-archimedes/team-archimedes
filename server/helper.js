eaf0bfd03c61f112523e81c57fb27c0c5778dde1
Ethan Lipkind <Ethan.lipkind@gmail.com>
2018-01-03 16:26:54 -0500

    fixed rebase issue

--- server/helper.js
+++ server/helper.js
@@ -1,24 +1,43 @@
-var OAuth = require ('oauth')
 
+///////////////////////////////////////////////////////////////////
+//
+// getTweet() does the work of querying the twitter API and then
+// executes the callback function from index.js with the results.
+//
+///////////////////////////////////////////////////////////////////
+
+var OAuth = require ('oauth');
+var key = require ('../config/twitter.js');
 
 module.exports.getTweet = (st, cb) => {
 	var oauth = new OAuth.OAuth(
 		'https://api.twitter.com/oauth/request_token',
 		'https://api.twitter.com/oauth/access_token',
-		window.CONSUMER_KEY,
-		window.CONSUMER_SECRET,
+		key.CONSUMER_KEY,
+		key.CONSUMER_SECRET,
 		'1.0A',
 		null,
 		'HMAC-SHA1'
 	);
 
-	oauth.get(`https://api.twitter.com/1.1/search/tweets.json?q=${st}`, window.ACCESS_TOKEN, window.ACCESS_TOKEN_SECRET, function(e, data, res) {
-		if (e) console.error(e)
-		cb(data)
-	})
-}
-<<<<<<< HEAD
-=======
+	oauth.get(`https://api.twitter.com/1.1/search/tweets.json?q=${st}&count=100`, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET, function(e, data, res) {
+		if (e) { console.error(e) }
+		let temp = JSON.parse(data).statuses
+		let cleaned = []
 
-// exports.getTweet('trump', (data) => {console.log(data)})
->>>>>>> added a bunch of stuff to the package.json folder in regards to styling
+		temp.map((tweet) => {
+			var selectedData = {
+				timeStamp: tweet.created_at,
+				tweetBody: tweet.retweeted_status ? tweet.retweeted_status.text : tweet.text, 
+				user_name: tweet.user.screen_name,
+				user_location: tweet.user.location,
+				avatar_url: tweet.user.profile_image_url
+			}
+
+			cleaned.push(selectedData)
+		});
+
+	cb(cleaned);
+
+	});
+}
