1f7f959d2237cd04173605914577d816f726611a
jessica wolvington <jessicawolvington@jessicas-MacBook-Pro.local>
2018-01-05 15:02:24 -0500

    deploy

---
+++ config/twitter.js
@@ -0,0 +1,4 @@
+module.exports.CONSUMER_KEY = 'PcKFVmwxY7UGi5r3TyWhy0hyE';
+module.exports.CONSUMER_SECRET = 'Vgr1vIIi3bMJqCZBtgTHqJf8vk72uhKR8nDCEzMNH5gPMFkTjp';
+module.exports.ACCESS_TOKEN = '751413619473027072-y34S4KyFxP5bdFuUFc8omIkUMn1ZwB4';
+module.exports.ACCESS_TOKEN_SECRET = 'lw2xmOIMc0ZWaOvNgkBeGu8zmjgDtvwpRrryA68tms0Lu';
