5a7ccd3bf29316a204a77aec18ce564376d32732
derricktheodoregit config --global user.email derrickxtheodore@gmail.comqlsqecslsesc <derrickxtheodore@gmail.com>
2018-01-16 10:41:50 -0500

    added backend routes for profile data GET

--- server/helper.js
+++ server/helper.js
@@ -94,5 +94,77 @@ getTweets = (st, cb) => {
 		});
 }
 
+getUserProfileData = (userScreenName, cb) => {
+	var oauth = new OAuth.OAuth(
+		'https://api.twitter.com/oauth/request_token',
+		'https://api.twitter.com/oauth/access_token',
+		key.CONSUMER_KEY,
+		key.CONSUMER_SECRET,
+		'1.0A',
+		null,
+		'HMAC-SHA1'
+	);
+
+	oauth.get(`https://api.twitter.com/1.1/search/show.json?screen_name=${userScreenName}&include_entities=false`, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET, function(e, data, res) {
+		if (e) { 
+			console.error(e);
+			cb([]);
+		} else {
+			//object
+			let userObject = JSON.parse(data)
+			//portected here
+			oauth.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?${userScreenName}&count=20&trim_user=trim&exclude_replies=true&include_rts=true`, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET, function(e, data, res) {
+				if (e) { 
+					console.error(e);
+					cb([]);
+				} else {
+					//array: all 3 default 20 
+					let statuses = JSON.parse(data)
+					//protected usr edge case
+					//retweets included
+					oauth.get(`https://api.twitter.com/1.1/followers/list.json?screen_name=${userScreenName}&skip_status=true&include_user_entities=false`, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET, function(e, data, res) {
+						if (e) { 
+							console.error(e);
+							cb([]);
+						} else {
+							let followers = JSON.parse(data).users
+							oauth.get(`https://api.twitter.com/1.1/friends/list.json?screen_name=${userScreenName}&skip_status=true&include_user_entities=false`, key.ACCESS_TOKEN, key.ACCESS_TOKEN_SECRET, function(e, data, res) {
+								if (e) { 
+									console.error(e);
+									cb([]);
+								} else {
+									let friends = JSON.parse(data).users
+									//parse user object
+									//iterate 20 times with array data and cover undefined keys
+								}
+							});
+						}
+					});
+				}
+			});
+		}
+	});
+}
+
 module.exports.getTweets = getTweets;
 module.exports.cronJob = cronJob;
+
+
+			// let temp = JSON.parse(data).statuses
+			// let cleaned = []
+			// cb(cleaned);
+
+			//Use to map data::
+			// temp.map((tweet) => {
+			// 	var selectedData = {
+			// 		// score: sentiment(tweet).score,
+			// 		searchTerm: st,
+			// 		timeStamp: tweet.created_at,
+			// 		// if tweet has been retweeted, its full text lives in the retweeted_status object
+			// 		tweetBody: tweet.retweeted_status ? tweet.retweeted_status.full_text : tweet.full_text,
+			// 		user_name: tweet.user.screen_name,
+			// 		user_location: tweet.user.location,
+			// 		avatar_url: tweet.user.profile_image_url
+			// 	}
+			// 	cleaned.push(selectedData)
+			// });
